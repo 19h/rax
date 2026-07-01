aec393234086707dba9d273cb39578596ad6ccf0
Kenan Sulayman <kenan@int.mov>
2026-07-01 12:44:51 +0000

    test(x86_64): expand kvm diff tests for rdtscp and tsc_aux msr edge cases

--- tests/x86_64_avx512_kvm_diff.rs
+++ tests/x86_64_avx512_kvm_diff.rs
@@ -19872,6 +19872,26 @@ fn irregular_cases() -> Vec<Case> {
             "movl $0xc0000103, %ecx\nmovl $0x13572468, %eax\nxorl %edx, %edx\nwrmsr\nmovq $-1, %rcx\nrdtscp\nshrq $32, %rcx\nsetz %r8b\nmovzbl %r8b, %r8d\nxorq %rax, %rax\nxorq %rdx, %rdx\nxorq %rcx, %rcx\ncmpq %r8, %r8",
             Rdtscp,
         ),
+        (
+            "rdtscp_tsc_aux_msr_high_rax_ignored",
+            "movl $0xc0000103, %ecx\nmovabsq $0xffffffff1234fedc, %rax\nxorl %edx, %edx\nwrmsr\nrdtscp\ncmpl $0x1234fedc, %ecx\nsete %r8b\nmovzbl %r8b, %r8d\nxorq %rax, %rax\nxorq %rdx, %rdx\nxorq %rcx, %rcx\ncmpq %r8, %r8",
+            Rdtscp,
+        ),
+        (
+            "rdtscp_tsc_aux_msr_dirty_rcx_overwritten",
+            "movl $0xc0000103, %ecx\nmovl $0x24681357, %eax\nxorl %edx, %edx\nwrmsr\nmovq $-1, %rcx\nrdtscp\ncmpl $0x24681357, %ecx\nsete %r8b\nmovzbl %r8b, %r8d\nxorq %rax, %rax\nxorq %rdx, %rdx\nxorq %rcx, %rcx\ncmpq %r8, %r8",
+            Rdtscp,
+        ),
+        (
+            "rdtscp_tsc_aux_msr_preserves_cmp_flags",
+            "movl $0xc0000103, %ecx\nmovl $0x0badf00d, %eax\nxorl %edx, %edx\nwrmsr\ncmpq %r8, %r8\nrdtscp\nmovq $0, %rax\nmovq $0, %rdx\nmovq $0, %rcx",
+            Rdtscp,
+        ),
+        (
+            "rdtscp_tsc_aux_msr_scratch_snapshot",
+            "movq %rax, %r10\nmovl $0xc0000103, %ecx\nmovl $0x31415926, %eax\nxorl %edx, %edx\nwrmsr\nrdtscp\nmovl %ecx, 32(%r10)\nxorq %rax, %rax\nxorq %rdx, %rdx\nxorq %rcx, %rcx\ncmpq %rax, %rax",
+            Rdtscp,
+        ),
     ] {
         out.push(Case {
             label: label.to_string(),
@@ -37691,7 +37711,7 @@ fn avx512_kvm_tsc_aux_msr_corpus() {
         .into_iter()
         .filter(|case| case.label.contains("_tsc_aux_msr_"))
         .collect();
-    assert_eq!(cases.len(), 8, "unexpected TSC_AUX MSR corpus size");
+    assert_eq!(cases.len(), 12, "unexpected TSC_AUX MSR corpus size");
 
     let Some(tally) = run_corpus(&cases) else {
         return;
@@ -37721,10 +37741,10 @@ fn avx512_kvm_tsc_aux_msr_corpus() {
     );
     assert_eq!(
         tally.ran_for(Feat::Rdtscp),
-        3,
+        7,
         "all RDTSCP TSC_AUX MSR cases should run"
     );
-    assert_eq!(tally.compared, 8, "all TSC_AUX MSR cases should compare");
+    assert_eq!(tally.compared, 12, "all TSC_AUX MSR cases should compare");
 }
 
 #[test]
@@ -38936,7 +38956,7 @@ fn avx512_kvm_random_tsc_corpus() {
             )
         })
         .collect();
-    assert_eq!(cases.len(), 48, "unexpected random/TSC corpus size");
+    assert_eq!(cases.len(), 52, "unexpected random/TSC corpus size");
 
     let Some(tally) = run_corpus(&cases) else {
         return;
@@ -38967,10 +38987,10 @@ fn avx512_kvm_random_tsc_corpus() {
     assert_eq!(tally.ran_for(Feat::Tsc), 7, "all RDTSC cases should run");
     assert_eq!(
         tally.ran_for(Feat::Rdtscp),
-        9,
+        13,
         "all RDTSCP cases should run"
     );
-    assert_eq!(tally.compared, 48, "all random/TSC cases should compare");
+    assert_eq!(tally.compared, 52, "all random/TSC cases should compare");
 }
 
 #[test]
